d1184114780477bca0210884435b30df1dc064e2
Alexinha J. Chang <alexchang@Alexinhas-MacBook-Air.local>
2024-09-08 18:29:02 -0400

    first commit

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const HeroSection = () => {
+  return (
+    <section>
+        <h1 className='text-white'>Alexina J. Chang's Portfolio Website</h1>
+    </section>
+  )
+}
+
+export default HeroSection;
